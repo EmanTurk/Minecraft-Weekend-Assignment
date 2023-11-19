1cc0fa3bd4211948acd4857a5deae4422c3ba520
EmanTurk <143930452+EmanTurk@users.noreply.github.com>
2023-11-19 18:29:40 +0200

    5 -5fith commit
    still having problems with the width, but will move to the functions

--- 2d-world.js
+++ 2d-world.js
@@ -12,30 +12,44 @@ function createWorld() {
       for (let j = 0; j < numCols; j++) {
         const cell = addCell();
   
-        // Change logic for bottom rows
-        if (i === numRows - 1) { // Last row (bottom)
+        // Bottom rows for diamonds and gold
+        if (i === numRows - 1) {
           cell.classList.add("diamond");
-        } else if (i === numRows - 2) { // Second to last row
+        } else if (i === numRows - 2) {
           cell.classList.add("gold");
-        } else if (i >= numRows - 8) { // Next 6 rows from the bottom
+        } else if (i >= numRows - 8) {
           let rndNum = Math.random();
           if (rndNum < 0.2) {
             cell.classList.add("gold");
           } else if (rndNum < 0.3) {
             cell.classList.add("diamond");
-          } else if (rndNum < 0.6) {
-            cell.classList.add("dirt");
           } else {
             cell.classList.add("dirt");
           }
-        } else if (i === 11) { //always row 11 for grass
+        } else if (i === 11) {
           cell.classList.add("grass");
         } else {
-          if (i > 2 && j >= 12 && j < 14) {
-            cell.classList.add("leaves");
-          } else if (i >= 6 && i <= 7 && j == 12) {
+          // First tree with wood and leaves
+          if (j === 12 && i >= 8 && i <= 10) {
             cell.classList.add("wood");
-          } else {
+          } else if ((i >= 5 && i <= 7) && (j >= 11 && j <= 13)) {
+            cell.classList.add("leaves");
+          } 
+          // Second tree with wood and flower-leaves
+          else if (j === numCols - 3 && i >= 8 && i <= 10) {
+            cell.classList.add("wood");
+          } else if ((i >= 6 && i <= 7) && (j >= numCols - 4 && j <= numCols - 2)) {
+            cell.classList.add("flower-leaves");
+          } 
+          if ((i >= 9 && i <= 10) && j < 3) {
+            cell.classList.add("stone");
+        }
+        // Small stone pile between trees
+        else if ((i >= 10 && i <= 10) && (j >= 15 && j <= 17)) {
+            cell.classList.add("stone");}
+          
+            
+          else {
             cell.classList.add("sky");
           }
         }
@@ -44,6 +58,8 @@ function createWorld() {
       }
     }
   }
+  
+  
   
 
 const addRow = () => {
