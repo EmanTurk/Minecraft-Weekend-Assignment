4c2c4032ac06b4465528426b187f3cdf5edfbd10
EmanTurk <143930452+EmanTurk@users.noreply.github.com>
2023-11-19 16:53:25 +0200

    4-forth commit
    Still having problems with the world, the container isn't taking the whole div container for some reason!

--- 2d-world.js
+++ 2d-world.js
@@ -1,43 +1,50 @@
-
+//========================================================================//    ---------*Build the 2d world- cell structure and placement*--------------- 
+//========================================================================//
 const numRows = 20;
 const numCols = 40;
 const gameWorld = document.getElementById("2d-world");
 
 function createWorld() {
-  for (let i = 0; i < numRows; i++) {
-    const row = addRow();
-    gameWorld.append(row);
-    for (let j = 0; j < numCols; j++) {
-      const cell = addCell();
-
-      if (i < 8) {
-        if (i > 2 && j >= 10 && j < 15) {
-          cell.classList.add("leaves");
-        } else if (i >= 6 && i <= 7 && j == 12) {
-          cell.classList.add("wood");
-        } else {
-          cell.classList.add("sky");
-        }
-      } else if (i == 8) {
-        cell.classList.add("grass");
-      } else {
-
-        let rndNum = Math.random();
-        if (rndNum < 0.05) {
-          cell.classList.add("gold");
-        } else if (rndNum < 0.01) {
+    for (let i = 0; i < numRows; i++) {
+      const row = addRow();
+      gameWorld.append(row);
+  
+      for (let j = 0; j < numCols; j++) {
+        const cell = addCell();
+  
+        // Change logic for bottom rows
+        if (i === numRows - 1) { // Last row (bottom)
           cell.classList.add("diamond");
-        } else if (rndNum < 0.6) {
-          cell.classList.add("stone");
+        } else if (i === numRows - 2) { // Second to last row
+          cell.classList.add("gold");
+        } else if (i >= numRows - 8) { // Next 6 rows from the bottom
+          let rndNum = Math.random();
+          if (rndNum < 0.2) {
+            cell.classList.add("gold");
+          } else if (rndNum < 0.3) {
+            cell.classList.add("diamond");
+          } else if (rndNum < 0.6) {
+            cell.classList.add("dirt");
+          } else {
+            cell.classList.add("dirt");
+          }
+        } else if (i === 11) { //always row 11 for grass
+          cell.classList.add("grass");
         } else {
-          cell.classList.add("dirt");
+          if (i > 2 && j >= 12 && j < 14) {
+            cell.classList.add("leaves");
+          } else if (i >= 6 && i <= 7 && j == 12) {
+            cell.classList.add("wood");
+          } else {
+            cell.classList.add("sky");
+          }
         }
+  
+        row.append(cell);
       }
-
-      row.append(cell);
     }
   }
-}
+  
 
 const addRow = () => {
   const row = document.createElement("div");
